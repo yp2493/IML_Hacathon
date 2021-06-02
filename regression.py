fd991bd11180cf93687535deefa44245dcddf30e
Yaniv Peeri <peeri123@gmail.com>
2021-06-02 20:31:03 +0300

    submit

---
+++ regression.py
@@ -0,0 +1,19 @@
+
+################################################
+#
+#     Task 1 - predict movies revenue & ranking
+#
+################################################
+
+
+def predict(csv_file):
+    """
+    This function predicts revenues and votes of movies given a csv file with movie details.
+    Note: Here you should also load your model since we are not going to run the training process.
+    :param csv_file: csv with movies details. Same format as the training dataset csv.
+    :return: a tuple - (a python list with the movies revenues, a python list with the movies avg_votes)
+    """
+
+    #your code goes here...
+
+    pass
