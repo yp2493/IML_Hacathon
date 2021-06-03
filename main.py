2d05000cf9c667f7dc16b82c23be7210ab4c824e
yp2493 <peeri123@gmail.com>
2021-06-03 10:42:54 +0300

    submit

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import pandas as pd
 import re
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_selection import SelectPercentile
 
 
-vectorizer=CountVectorizer(ngram_range=(1,2))
-corpus = [
-    'This is the first document.',
-    'This is the second second document.',
-    'And the third one.',
-    'Is this the first document?',
-    "Is this"
-]
-x = vectorizer.fit_transform(corpus)
-print(x.toarray().shape)
-transformer=TfidfTransformer(smooth_idf=False)
-tf=transformer.fit_transform(x.toarray())
-print(tf.toarray().shape)
-print(x.toarray())
-print(tf.toarray())
+vectorizer=TfidfVectorizer(ngram_range=(1,2), decode_error="replace")
+data=pd.read_csv("movies_dataset.csv")
+# x=vectorizer.fit_transform(data["overview"])
+# print(x.shape)
+# print(data["overview"])
+x=vectorizer.fit_transform(data["title"])
 
+
+
+def bagOfWords(column, movies):
+    data=movies[column]
+    vectorizer=TfidfVectorizer(ngram_range=(1,2), decode_error="replace")
+    return vectorizer.fit_transform(data).toarray()
+
+
+def dim_reduction(bag_of_words):
+    p=SelectPercentile( percentile=50)
+    return p.fit_transform(bag_of_words)
+
+
+def join_bag_of_words(bag_of_words, data, column):
+    data=data[:column].join(bag_of_words).join(data[column:])
+    data.pop("column")
+    return data
+
+
+
+words=bagOfWords("title", data)
+words2=dim_reduction(words)
+print(words2.shape)
+print(words.shape)
+# d=join_bag_of_words(words, data, "title")
